ff03acee768cefbd78d4ec408e4ada366dccd93c
JGrum <jgrumby@hotmail.com>
2023-10-02 08:50:25 -0400

    Documentation updates; no logic changes.

--- ECMOPressureRead.ino
+++ ECMOPressureRead.ino
@@ -1,30 +1,12 @@
-/*
-  Digital Pot Control
+/*********************************************************************************
 
-  This example controls an Analog Devices AD5206 digital potentiometer.
-  The AD5206 has 6 potentiometer channels. Each channel's pins are labeled
-  A - connect this to voltage
-  W - this is the pot's wiper, which changes when you set it
-  B - connect this to ground.
+  ECMO Pressure Measurement Prototype
+    reads pressure across ECMO device for early-stage detection 
+	of clotting/clogging
 
- The AD5206 is SPI-compatible,and to command it, you send two bytes,
- one with the channel number (0 - 5) and one with the resistance value for the
- channel (0 - 255).
+  ECMO VIP Team - Stony Brook University
 
- The circuit:
-  * All A pins  of AD5206 connected to +5V
-  * All B pins of AD5206 connected to ground
-  * An LED and a 220-ohm resistor in series connected from each W pin to ground
-  * CS - to digital pin 10  (SS pin)
-  * SDI - to digital pin 11 (MOSI pin)
-  * CLK - to digital pin 13 (SCK pin)
-
- created 10 Aug 2010
- by Tom Igoe
-
- Thanks to Heather Dewey-Hagborg for the original tutorial, 2005
-
-*/
+**********************************************************************************/
 
 
 // include the SPI library:
